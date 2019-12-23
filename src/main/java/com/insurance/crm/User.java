faab1c0b9f16de556bb19d1c754852026789962d
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2019-12-23 16:21:35 +0200

    Created User class

--- src/main/java/com/insurance/crm/User.java
+++ src/main/java/com/insurance/crm/User.java
@@ -1,9 +1,102 @@
 package com.insurance.crm;
 
+import java.util.Set;
+
 public class User {
     private Long id;
-    private String sign_date;
-    private String expiry_date;
-    private Filiation filiation;
+    private String surname;
+    private String firstname;
+    private String patronymic;
+    private String login;
+    private String password;
+    private Integer age;
+    private boolean active;
+    private Set<Role> roles;
 
+    public User(Long id, String surname, String firstname, String patronymic, String login, String password, Integer age, boolean active, Set<Role> roles) {
+        this.id = id;
+        this.surname = surname;
+        this.firstname = firstname;
+        this.patronymic = patronymic;
+        this.login = login;
+        this.password = password;
+        this.age = age;
+        this.active = active;
+        this.roles = roles;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
