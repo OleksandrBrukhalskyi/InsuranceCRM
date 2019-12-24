dbb8e1ff7867797ae14c2e490c05c95060d0055e
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2019-12-24 16:00:49 +0200

    Added cascade type into Agent class

--- src/main/java/com/insurance/crm/entity/Agent.java
+++ src/main/java/com/insurance/crm/entity/Agent.java
@@ -4,10 +4,7 @@ import com.insurance.crm.entity.enums.Role;
 import lombok.*;
 import org.aspectj.lang.annotation.DeclareAnnotation;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.List;
 import java.util.Set;
 @Entity
@@ -18,9 +15,9 @@ import java.util.Set;
 @Builder
 @Table(name = "agents")
 public class Agent extends User {
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Filiation filiation;
-    @OneToMany(mappedBy = "agent")
+    @OneToMany(mappedBy = "agent",cascade = CascadeType.ALL)
     private List<InsurancePolicy> insurancePolicies;
 
 
