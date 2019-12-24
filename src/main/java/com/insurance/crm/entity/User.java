75916e7ba3e8aa191c7f65d8921da5148d0ed532
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2019-12-24 11:54:47 +0200

    Added anotations to User class.

--- src/main/java/com/insurance/crm/entity/User.java
+++ src/main/java/com/insurance/crm/entity/User.java
@@ -1,119 +1,45 @@
 package com.insurance.crm.entity;
 
 import com.insurance.crm.entity.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, length = 20)
     private String surname;
+    @Column(nullable = false, length = 20)
     private String firstname;
+    @Column(nullable = false, length = 20)
     private String patronymic;
+    @Column(nullable = false, length = 50, unique = true)
+    private String email;
+    @Column(nullable = false, length = 20)
     private String login;
+    @Column(nullable = false, length = 20)
     private String password;
+    @Column(nullable = false, length = 2)
     private Integer age;
+    @Column(name = "is_active",nullable = false)
     private boolean active;
+    @ElementCollection(targetClass = Role.class,fetch = FetchType.EAGER)
+    @CollectionTable(name="user_role",joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
-    public User(Long id, String surname, String firstname, String patronymic, String login, String password, Integer age, boolean active, Set<Role> roles) {
-        this.id = id;
-        this.surname = surname;
-        this.firstname = firstname;
-        this.patronymic = patronymic;
-        this.login = login;
-        this.password = password;
-        this.age = age;
-        this.active = active;
-        this.roles = roles;
-    }
 
-    public User() {
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getPatronymic() {
-        return patronymic;
-    }
-
-    public void setPatronymic(String patronymic) {
-        this.patronymic = patronymic;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", surname='" + surname + '\'' +
-                ", firstname='" + firstname + '\'' +
-                ", patronymic='" + patronymic + '\'' +
-                ", login='" + login + '\'' +
-                ", password='" + password + '\'' +
-                ", age=" + age +
-                ", active=" + active +
-                ", roles=" + roles +
-                '}';
-    }
 }
