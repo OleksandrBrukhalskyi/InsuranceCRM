43d2854da35273e2ce1be1df0111fbc2d3c40a86
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2019-12-24 15:26:31 +0200

    Added anotations to InsurancePolicy class.

--- src/main/java/com/insurance/crm/entity/InsurancePolicy.java
+++ src/main/java/com/insurance/crm/entity/InsurancePolicy.java
@@ -1,12 +1,31 @@
 package com.insurance.crm.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import javax.xml.ws.BindingType;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
 public class InsurancePolicy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false,length = 10)
     private String signDate;
+    @Column(nullable = false,length = 10)
     private String expiryDate;
+    @ManyToOne
     private Filiation filiation;
+    @OneToOne
     private InsuranceType insuranceType;
+    @ManyToOne
     private Agent agent;
+    @ManyToOne
     private Customer customer;
 
 
