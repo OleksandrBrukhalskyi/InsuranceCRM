d00048138f55453f4eeea8625c8b875c3b99c5ed
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2019-12-24 13:15:24 +0200

    Added anotations to Agent class.

--- src/main/java/com/insurance/crm/entity/Agent.java
+++ src/main/java/com/insurance/crm/entity/Agent.java
@@ -1,40 +1,28 @@
 package com.insurance.crm.entity;
 
 import com.insurance.crm.entity.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.aspectj.lang.annotation.DeclareAnnotation;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.Set;
-
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "agents")
 public class Agent extends User {
+    @ManyToOne
     private Filiation filiation;
+    @OneToMany(mappedBy = "agent")
     private InsurancePolicy insurancePolicy;
 
-    public Agent(Long id, String surname, String firstname, String patronymic, String login, String password, Integer age, boolean active, Set<Role> roles, Filiation filiation, InsurancePolicy insurancePolicy) {
-        super(id, surname, firstname, patronymic, login, password, age, active, roles);
-        this.filiation = filiation;
-        this.insurancePolicy = insurancePolicy;
-    }
 
-    public Filiation getFiliation() {
-        return filiation;
-    }
-
-    public void setFiliation(Filiation filiation) {
-        this.filiation = filiation;
-    }
-
-    public InsurancePolicy getInsurancePolicy() {
-        return insurancePolicy;
-    }
-
-    public void setInsurancePolicy(InsurancePolicy insurancePolicy) {
-        this.insurancePolicy = insurancePolicy;
-    }
-
-    @Override
-    public String toString() {
-        return "Agent{" +
-                "filiation=" + filiation +
-                ", insurancePolicy=" + insurancePolicy +
-                '}';
-    }
 }
