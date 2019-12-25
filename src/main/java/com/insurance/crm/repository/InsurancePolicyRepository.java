fe69322884636eacd9bc36f40ff47bb2da30a88d
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2019-12-25 17:10:58 +0200

    Extends JpaRepository to InsurancePolicy.

--- src/main/java/com/insurance/crm/repository/InsurancePolicyRepository.java
+++ src/main/java/com/insurance/crm/repository/InsurancePolicyRepository.java
@@ -1,4 +1,7 @@
 package com.insurance.crm.repository;
 
-public interface InsurancePolicyRepository {
+import com.insurance.crm.entity.InsurancePolicy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InsurancePolicyRepository extends JpaRepository<InsurancePolicy,Long> {
 }
