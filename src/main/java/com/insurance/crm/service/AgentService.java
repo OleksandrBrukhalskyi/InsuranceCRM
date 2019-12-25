c640194710510e5020a93050d16ed72d0b41ad1e
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2019-12-25 19:58:05 +0200

    Added abstract methods in AgentService interface.

--- src/main/java/com/insurance/crm/service/AgentService.java
+++ src/main/java/com/insurance/crm/service/AgentService.java
@@ -1,4 +1,14 @@
 package com.insurance.crm.service;
 
+import com.insurance.crm.entity.Agent;
+
+import java.util.List;
+
 public interface AgentService {
+    List<Agent> getAgents();
+    Agent create(Agent agent);
+    Agent update(Agent agent);
+    void delete(Long id);
+    Agent getById(Long id);
+    Agent findByAgentSurname(String surname);
 }
