700b40fc799494d7b12ed0db9684715f3e395b29
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2019-12-25 20:01:35 +0200

    Added getAllAgents() method in AgentService implementation.

--- src/main/java/com/insurance/crm/service/impl/AgentServiceImpl.java
+++ src/main/java/com/insurance/crm/service/impl/AgentServiceImpl.java
@@ -1,4 +1,17 @@
 package com.insurance.crm.service.impl;
 
+import com.insurance.crm.entity.Agent;
+import com.insurance.crm.service.AgentService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class AgentServiceImpl {
+    @Autowired
+    AgentService agentService;
+
+    public List<Agent> findAllAgents(){
+        return agentService.getAgents();
+    }
+
 }
