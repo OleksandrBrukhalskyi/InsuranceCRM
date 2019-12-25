48606276a182422dfb75e51b25f251b827552e0e
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2019-12-25 17:12:42 +0200

    Extends JpaRepository to AgentRepository.

--- src/main/java/com/insurance/crm/repository/UserRepository.java
+++ src/main/java/com/insurance/crm/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.insurance.crm.repository;
 
-public interface UserRepository {
+import com.insurance.crm.entity.Agent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Agent,Long> {
 }
