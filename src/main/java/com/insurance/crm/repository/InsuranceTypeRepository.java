3573a925791ced24ff7a5ae6689f08b5e8eacfee
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2019-12-26 14:04:49 +0200

    Added abstract methods into InsuranceTypeService

--- src/main/java/com/insurance/crm/repository/InsuranceTypeRepository.java
+++ src/main/java/com/insurance/crm/repository/InsuranceTypeRepository.java
@@ -4,7 +4,10 @@ import com.insurance.crm.entity.InsuranceType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InsuranceTypeRepository extends JpaRepository<InsuranceType,Long> {
     InsuranceType findByName(String name);
+    List<InsuranceType> getInsuranceTypeByName(String name);
 }
