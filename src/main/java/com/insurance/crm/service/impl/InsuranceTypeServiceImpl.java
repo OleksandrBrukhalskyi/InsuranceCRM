515aeb28e977303ceaab641d8dc5fef068bb6a58
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2019-12-26 14:07:45 +0200

    Implemented methods into InsuranceTypeService.

--- src/main/java/com/insurance/crm/service/impl/InsuranceTypeServiceImpl.java
+++ src/main/java/com/insurance/crm/service/impl/InsuranceTypeServiceImpl.java
@@ -1,7 +1,46 @@
 package com.insurance.crm.service.impl;
 
+import com.insurance.crm.entity.InsuranceType;
+import com.insurance.crm.repository.InsuranceTypeRepository;
+import com.insurance.crm.service.InsuranceTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class InsuranceTypeServiceImpl {
+public class InsuranceTypeServiceImpl implements InsuranceTypeService {
+    @Autowired
+    InsuranceTypeRepository insuranceTypeRepository;
+
+    @Override
+    public List<InsuranceType> getInsuranceTypes() {
+        return insuranceTypeRepository.findAll();
+    }
+
+    @Override
+    public InsuranceType create(InsuranceType insuranceType) {
+        return insuranceTypeRepository.save(insuranceType);
+    }
+
+    @Override
+    public InsuranceType update(InsuranceType insuranceType) {
+        return insuranceTypeRepository.save(insuranceType);
+    }
+
+    @Override
+    public void delete(Long id) {
+        insuranceTypeRepository.deleteById(id);
+    }
+
+    @Override
+    public Optional<InsuranceType> findById(Long id) {
+        return insuranceTypeRepository.findById(id);
+    }
+
+    @Override
+    public List<InsuranceType> getInsuranceTypeByName(String name) {
+        return insuranceTypeRepository.getInsuranceTypeByName(name);
+    }
 }
