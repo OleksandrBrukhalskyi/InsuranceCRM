4830f1ad7eaf4eefb2fca58c6637a77d86cae256
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2019-12-26 13:15:13 +0200

    Implemented methods into CustomerService.

--- src/main/java/com/insurance/crm/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/insurance/crm/service/impl/CustomerServiceImpl.java
@@ -1,7 +1,46 @@
 package com.insurance.crm.service.impl;
 
+import com.insurance.crm.entity.Customer;
+import com.insurance.crm.repository.CustomerRepository;
+import com.insurance.crm.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class CustomerServiceImpl {
+public class CustomerServiceImpl implements CustomerService {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Override
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer create(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public Customer update(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public void delete(Long id) {
+        customerRepository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Customer> getById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public Customer findByCustomerSurname(String surname) {
+        return customerRepository.findCustomerBySurname(surname);
+    }
 }
