0be6e65ea27e9050b2dff22d4c0a9b5152cb1101
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2019-12-26 13:18:48 +0200

    Added abstract methods into FiliationService

--- src/main/java/com/insurance/crm/service/FiliationService.java
+++ src/main/java/com/insurance/crm/service/FiliationService.java
@@ -1,4 +1,13 @@
 package com.insurance.crm.service;
 
+import com.insurance.crm.entity.Filiation;
+
+import java.util.List;
+
 public interface FiliationService {
+    List<Filiation> getFiliations();
+    Filiation create(Filiation filiation);
+    Filiation update(Filiation filiation);
+    void delete(Long id);
+    Filiation findById(Long id);
 }
