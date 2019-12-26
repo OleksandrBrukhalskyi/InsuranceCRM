b1b77d0fa66506ebf2df9a2ef89b2f68c6a8ab48
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2019-12-26 13:22:07 +0200

    Implemented methods into FiliationService.

--- src/main/java/com/insurance/crm/service/FiliationService.java
+++ src/main/java/com/insurance/crm/service/FiliationService.java
@@ -3,11 +3,12 @@ package com.insurance.crm.service;
 import com.insurance.crm.entity.Filiation;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FiliationService {
     List<Filiation> getFiliations();
     Filiation create(Filiation filiation);
     Filiation update(Filiation filiation);
     void delete(Long id);
-    Filiation findById(Long id);
+    Optional<Filiation> findById(Long id);
 }
--- src/main/java/com/insurance/crm/service/impl/FiliationServiceImpl.java
+++ src/main/java/com/insurance/crm/service/impl/FiliationServiceImpl.java
@@ -1,7 +1,41 @@
 package com.insurance.crm.service.impl;
 
+import com.insurance.crm.entity.Filiation;
+import com.insurance.crm.repository.FiliationRepository;
+import com.insurance.crm.service.FiliationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class FiliationServiceImpl {
+public class FiliationServiceImpl implements FiliationService {
+    @Autowired
+    FiliationRepository filiationRepository;
+
+    @Override
+    public List<Filiation> getFiliations() {
+        return filiationRepository.findAll();
+    }
+
+    @Override
+    public Filiation create(Filiation filiation) {
+        return filiationRepository.save(filiation);
+    }
+
+    @Override
+    public Filiation update(Filiation filiation) {
+        return filiationRepository.save(filiation);
+    }
+
+    @Override
+    public void delete(Long id) {
+        filiationRepository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Filiation> findById(Long id) {
+        return filiationRepository.findById(id);
+    }
 }
