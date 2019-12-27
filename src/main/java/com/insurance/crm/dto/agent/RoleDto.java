c13b9875bfe13d391afcdce0e53a3d4f9eb459aa
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2019-12-27 10:51:42 +0200

    Added field roles to RoleDto

--- src/main/java/com/insurance/crm/dto/agent/RoleDto.java
+++ src/main/java/com/insurance/crm/dto/agent/RoleDto.java
@@ -1,4 +1,11 @@
 package com.insurance.crm.dto.agent;
 
+import com.insurance.crm.entity.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class RoleDto {
+    private Role[] roles;
 }
