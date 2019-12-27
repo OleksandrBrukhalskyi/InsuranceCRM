6bda2704ba6834406e173201de49d67a7953b34a
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2019-12-27 19:32:20 +0200

    Added fields into InsurancePolicyUpdateDto

--- src/main/java/com/insurance/crm/dto/policy/InsurancePolicyUpdateDto.java
+++ src/main/java/com/insurance/crm/dto/policy/InsurancePolicyUpdateDto.java
@@ -1,4 +1,21 @@
 package com.insurance.crm.dto.policy;
 
+import com.insurance.crm.dto.agent.AgentCreationDto;
+import com.insurance.crm.dto.filiation.FiliationDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class InsurancePolicyUpdateDto {
+    @NotBlank
+    private Long id;
+    @NotBlank
+    private FiliationDto filiationDto;
+    @NotBlank
+    private AgentCreationDto agentDto;
 }
