377e3faf5a00828f621668d0d7f478f5d9c4f914
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2019-12-27 19:34:05 +0200

    Added fields into InsuranceTypeUpdateDto

--- src/main/java/com/insurance/crm/dto/type/InsuranceTypeUpdateDto.java
+++ src/main/java/com/insurance/crm/dto/type/InsuranceTypeUpdateDto.java
@@ -1,4 +1,21 @@
 package com.insurance.crm.dto.type;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class InsuranceTypeUpdateDto {
+    @NotBlank
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String tag;
+    @NotBlank
+    private Double sumInsured;
 }
