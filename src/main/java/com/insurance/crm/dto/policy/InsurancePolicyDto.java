e94362da80d99e3cf4384656a4b03dcab6ae6a02
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2019-12-27 12:57:35 +0200

    Created Agent and InsurancePolicy Dto's.

--- src/main/java/com/insurance/crm/dto/policy/InsurancePolicyDto.java
+++ src/main/java/com/insurance/crm/dto/policy/InsurancePolicyDto.java
@@ -1,4 +1,17 @@
 package com.insurance.crm.dto.policy;
 
+import com.insurance.crm.dto.filiation.FiliationDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class InsurancePolicyDto {
+    private Long id;
+    private String signDate;
+    private String expiryDate;
+    private FiliationDto filiationDto;
+
 }
