a01bd26651f7bd32a6d0eac1d9aec450cf4f0c47
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2019-12-27 10:53:18 +0200

    Added fields to AgentRoleDto

--- src/main/java/com/insurance/crm/dto/agent/AgentRoleDto.java
+++ src/main/java/com/insurance/crm/dto/agent/AgentRoleDto.java
@@ -1,4 +1,19 @@
 package com.insurance.crm.dto.agent;
 
+import com.insurance.crm.entity.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AgentRoleDto {
+    @NotNull
+    private Long id;
+
+    @NotNull
+    private Role role;
 }
