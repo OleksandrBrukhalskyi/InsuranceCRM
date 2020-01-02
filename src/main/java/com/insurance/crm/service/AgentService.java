5cc3d642b88d3e0009ee6e76917d562e983d5550
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-02 14:50:13 +0200

    Added updateStatus,getRoles methods into AgentService.

--- src/main/java/com/insurance/crm/service/AgentService.java
+++ src/main/java/com/insurance/crm/service/AgentService.java
@@ -1,14 +1,24 @@
 package com.insurance.crm.service;
 
+import com.insurance.crm.dto.agent.AgentRoleDto;
+import com.insurance.crm.dto.agent.AgentStatusDto;
+import com.insurance.crm.dto.agent.AgentUpdateDto;
+import com.insurance.crm.dto.agent.RoleDto;
 import com.insurance.crm.entity.Agent;
+import com.insurance.crm.entity.enums.AgentStatus;
+import com.insurance.crm.entity.enums.Role;
 
 import java.util.List;
 
 public interface AgentService {
     List<Agent> getAgents();
     Agent create(Agent agent);
-    Agent update(Agent agent);
+    Agent update(AgentUpdateDto agent,Long id);
     void delete(Long id);
     Agent getById(Long id);
     Agent findByAgentSurname(String surname);
+    AgentStatusDto updateStatus(Long id, AgentStatus agentStatus);
+    RoleDto getRoles();
+    AgentRoleDto updateRole(Long id, Role role);
+
 }
--- src/main/java/com/insurance/crm/service/impl/AgentServiceImpl.java
+++ src/main/java/com/insurance/crm/service/impl/AgentServiceImpl.java
@@ -1,8 +1,12 @@
 package com.insurance.crm.service.impl;
 
+import com.insurance.crm.dto.agent.AgentRoleDto;
 import com.insurance.crm.dto.agent.AgentStatusDto;
+import com.insurance.crm.dto.agent.AgentUpdateDto;
+import com.insurance.crm.dto.agent.RoleDto;
 import com.insurance.crm.entity.Agent;
 import com.insurance.crm.entity.enums.AgentStatus;
+import com.insurance.crm.entity.enums.Role;
 import com.insurance.crm.exception.BadIdException;
 import com.insurance.crm.repository.AgentRepository;
 import com.insurance.crm.service.AgentService;
@@ -31,9 +35,19 @@ public class AgentServiceImpl implements AgentService {
     public Agent create(Agent agent){
         return agentRepository.save(agent);
     }
-    public Agent update(Agent agent){
-        return agentRepository.save(agent);
+
+    @Override
+    public Agent update(AgentUpdateDto dto,Long id) {
+        Agent agent = agentRepository
+                .findById(id)
+                .orElseThrow(() -> new BadIdException(AGENT_NOT_FOUND_BY_ID + id));
+        agent.setSurname(dto.getSurname());
+        agent.setFirstname(dto.getFirstname());
+        agent.setPatronymic(dto.getPatronymic());
+        return null;
     }
+
+
     public void delete(Long id){
         agentRepository.deleteById(id);
     }
@@ -48,10 +62,21 @@ public class AgentServiceImpl implements AgentService {
     public Agent findByAgentSurname(String surname) {
         return agentRepository.findAgentBySurname(surname);
     }
+    @Override
     public AgentStatusDto updateStatus(Long id, AgentStatus agentStatus){
         Agent agent = getById(id);
         agent.setAgentStatus(agentStatus);
         return modelMapper.map(agentRepository.save(agent),AgentStatusDto.class);
+    }
+
+    @Override
+    public RoleDto getRoles() {
+        return new RoleDto(Role.class.getEnumConstants());
+    }
+
+    @Override
+    public AgentRoleDto updateRole(Long id, Role role) {
+        return null;
     }
 
 }
