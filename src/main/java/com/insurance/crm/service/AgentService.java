fd427958a993a269f5b661131085914e99d7ab71
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-02 14:32:01 +0200

    Added updateStatus method into AgentService.

--- src/main/java/com/insurance/crm/service/AgentService.java
+++ src/main/java/com/insurance/crm/service/AgentService.java
@@ -3,13 +3,12 @@ package com.insurance.crm.service;
 import com.insurance.crm.entity.Agent;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface AgentService {
     List<Agent> getAgents();
     Agent create(Agent agent);
     Agent update(Agent agent);
     void delete(Long id);
-    Optional<Agent> getById(Long id);
+    Agent getById(Long id);
     Agent findByAgentSurname(String surname);
 }
--- src/main/java/com/insurance/crm/service/impl/AgentServiceImpl.java
+++ src/main/java/com/insurance/crm/service/impl/AgentServiceImpl.java
@@ -1,16 +1,25 @@
 package com.insurance.crm.service.impl;
 
+import com.insurance.crm.dto.agent.AgentStatusDto;
 import com.insurance.crm.entity.Agent;
+import com.insurance.crm.entity.enums.AgentStatus;
+import com.insurance.crm.exception.BadIdException;
 import com.insurance.crm.repository.AgentRepository;
 import com.insurance.crm.service.AgentService;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
+import static com.insurance.crm.constant.ErrorMessage.AGENT_NOT_FOUND_BY_ID;
+@Service
+@AllArgsConstructor
 public class AgentServiceImpl implements AgentService {
     @Autowired
     AgentRepository agentRepository;
+    private ModelMapper modelMapper;
 
 
 
@@ -30,13 +39,19 @@ public class AgentServiceImpl implements AgentService {
     }
 
     @Override
-    public Optional<Agent> getById(Long id) {
-        return agentRepository.findById(id);
+    public Agent getById(Long id) {
+        return agentRepository.findById(id)
+                .orElseThrow(()-> new BadIdException(AGENT_NOT_FOUND_BY_ID + id));
     }
 
     @Override
     public Agent findByAgentSurname(String surname) {
         return agentRepository.findAgentBySurname(surname);
+    }
+    public AgentStatusDto updateStatus(Long id, AgentStatus agentStatus){
+        Agent agent = getById(id);
+        agent.setAgentStatus(agentStatus);
+        return modelMapper.map(agentRepository.save(agent),AgentStatusDto.class);
     }
 
 }
