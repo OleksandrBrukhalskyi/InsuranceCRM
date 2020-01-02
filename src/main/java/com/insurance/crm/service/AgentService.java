087906ca2e3b62cf684facc68f5524df51cb50c0
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-02 16:16:14 +0200

    Added getByEmail method into AgentService.

--- src/main/java/com/insurance/crm/service/AgentService.java
+++ src/main/java/com/insurance/crm/service/AgentService.java
@@ -13,9 +13,10 @@ import java.util.List;
 public interface AgentService {
     List<Agent> getAgents();
     Agent create(Agent agent);
-    Agent update(AgentUpdateDto agent,Long id);
+    Agent update(AgentUpdateDto agent,String email);
     void delete(Long id);
     Agent getById(Long id);
+    Agent getByEmail(String email);
     Agent findByAgentSurname(String surname);
     AgentStatusDto updateStatus(Long id, AgentStatus agentStatus);
     RoleDto getRoles();
