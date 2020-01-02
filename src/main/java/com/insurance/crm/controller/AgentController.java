fe0f293f296b5036b8d439ee0bac8a5d6ff97e9c
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-02 15:33:56 +0200

    Have created updateRole and getRoles methods in AgentController.

--- src/main/java/com/insurance/crm/controller/AgentController.java
+++ src/main/java/com/insurance/crm/controller/AgentController.java
@@ -1,7 +1,9 @@
 package com.insurance.crm.controller;
 
 import com.insurance.crm.constant.HttpStatuses;
+import com.insurance.crm.dto.agent.AgentRoleDto;
 import com.insurance.crm.dto.agent.AgentStatusDto;
+import com.insurance.crm.dto.agent.RoleDto;
 import com.insurance.crm.entity.enums.AgentStatus;
 import com.insurance.crm.service.impl.AgentServiceImpl;
 import io.swagger.annotations.ApiOperation;
@@ -10,10 +12,7 @@ import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.Valid;
@@ -40,4 +39,30 @@ public class AgentController {
                         agentStatusDto.getId(),agentStatusDto.getAgentStatus()));
 
     }
+    @ApiOperation(value = "Update role of agent")
+    @ApiResponses(value = {
+            @ApiResponse(code=200, message = HttpStatuses.OK,response = AgentRoleDto.class),
+            @ApiResponse(code=303, message = HttpStatuses.SEE_OTHER),
+            @ApiResponse(code =400,message = HttpStatuses.BAD_REQUEST),
+            @ApiResponse(code=403,message = HttpStatuses.FORBIDDEN)
+    })
+    @PatchMapping("role")
+    public ResponseEntity<AgentRoleDto> updateRole(
+            @Valid @RequestBody AgentRoleDto agentRoleDto,@ApiIgnore Principal principal){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(agentService.updateRole(
+                                agentRoleDto.getId(),agentRoleDto.getRole()));
+    }
+    @ApiOperation(value = "Get all roles")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = HttpStatuses.OK, response = RoleDto.class),
+            @ApiResponse(code = 303, message = HttpStatuses.SEE_OTHER),
+            @ApiResponse(code = 403, message = HttpStatuses.FORBIDDEN)
+    })
+    @GetMapping("roles")
+    public ResponseEntity<RoleDto> getRoles(){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(agentService.getRoles());
+    }
+
 }
