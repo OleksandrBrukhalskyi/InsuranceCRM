b759e127e8bdc6293bf38fab580b787fc11bf032
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-02 16:08:52 +0200

    Added AGENT_NOT_FOUND_BY_EMAIL error message.

--- src/main/java/com/insurance/crm/constant/ErrorMessage.java
+++ src/main/java/com/insurance/crm/constant/ErrorMessage.java
@@ -2,4 +2,5 @@ package com.insurance.crm.constant;
 
 public class ErrorMessage {
     public static final String AGENT_NOT_FOUND_BY_ID = "The agent does not exist by this id: ";
+    public static final String AGENT_NOT_FOUND_BY_EMAIL = "The agent does not exist by this email: ";
 }
