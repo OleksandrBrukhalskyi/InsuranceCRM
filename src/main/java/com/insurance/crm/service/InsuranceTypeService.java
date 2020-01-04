6a0e36e55f6499350813dc181f509df9633e42a3
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 04:50:27 +0200

    Added InsuranceTypeUpdateDto as parameter to InsuranceType update method.

--- src/main/java/com/insurance/crm/service/InsuranceTypeService.java
+++ src/main/java/com/insurance/crm/service/InsuranceTypeService.java
@@ -1,5 +1,6 @@
 package com.insurance.crm.service;
 
+import com.insurance.crm.dto.type.InsuranceTypeUpdateDto;
 import com.insurance.crm.entity.InsuranceType;
 
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.Optional;
 public interface InsuranceTypeService {
     List<InsuranceType> getInsuranceTypes();
     InsuranceType create(InsuranceType insuranceType);
-    InsuranceType update(InsuranceType insuranceType);
+    InsuranceType update(InsuranceTypeUpdateDto insuranceType,Long id);
     void delete(Long id);
     Optional<InsuranceType> findById(Long id);
     List<InsuranceType> getInsuranceTypeByName(String name);
