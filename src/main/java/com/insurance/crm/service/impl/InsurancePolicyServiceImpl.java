99e32ba96074d970d499df431687f9d97ef220c9
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 05:24:43 +0200

    Implemented update method in InsurancePolicyService.

--- src/main/java/com/insurance/crm/service/impl/InsurancePolicyServiceImpl.java
+++ src/main/java/com/insurance/crm/service/impl/InsurancePolicyServiceImpl.java
@@ -1,6 +1,8 @@
 package com.insurance.crm.service.impl;
 
+import com.insurance.crm.dto.policy.InsurancePolicyUpdateDto;
 import com.insurance.crm.entity.InsurancePolicy;
+import com.insurance.crm.exception.BadIdException;
 import com.insurance.crm.repository.InsurancePolicyRepository;
 import com.insurance.crm.service.InsurancePolicyService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
+import static com.insurance.crm.constant.ErrorMessage.INSURANCE_POLICY_NOT_FOUND_BY_ID;
 
 @Service
 public class InsurancePolicyServiceImpl implements InsurancePolicyService {
@@ -25,9 +29,15 @@ public class InsurancePolicyServiceImpl implements InsurancePolicyService {
     }
 
     @Override
-    public InsurancePolicy update(InsurancePolicy insurancePolicy) {
+    public InsurancePolicy update(InsurancePolicyUpdateDto dto, Long id) {
+        InsurancePolicy insurancePolicy = insurancePolicyRepository.findById(id)
+                .orElseThrow(()-> new BadIdException(INSURANCE_POLICY_NOT_FOUND_BY_ID + id));
+        insurancePolicy.setFiliation(dto.getFiliation());
+        insurancePolicy.setAgent(dto.getAgent());
         return insurancePolicyRepository.save(insurancePolicy);
     }
+
+
 
     @Override
     public void delete(Long id) {
