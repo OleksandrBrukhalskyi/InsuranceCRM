b6817af95ed60a84a4e58bd43b55850a09c2d4a7
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 15:43:44 +0200

    Added logging in save method.

--- src/main/java/com/insurance/crm/service/impl/FiliationServiceImpl.java
+++ src/main/java/com/insurance/crm/service/impl/FiliationServiceImpl.java
@@ -1,5 +1,6 @@
 package com.insurance.crm.service.impl;
 
+import com.insurance.crm.constant.LogMessage;
 import com.insurance.crm.dto.filiation.FiliationDto;
 import com.insurance.crm.entity.Filiation;
 import com.insurance.crm.exception.BadIdException;
@@ -8,6 +9,7 @@ import com.insurance.crm.service.FiliationService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,12 +27,15 @@ public class FiliationServiceImpl implements FiliationService {
     private ModelMapper modelMapper;
 
     @Override
-    public List<Filiation> getFiliations() {
-        return filiationRepository.findAll();
+    public List<FiliationDto> getFiliations() {
+        log.info(LogMessage.IN_FIND_ALL);
+        return modelMapper.map(filiationRepository.findAll(), new TypeToken<List<FiliationDto>>(){
+        }.getType());
     }
 
     @Override
     public Filiation create(Filiation filiation) {
+        log.info(LogMessage.IN_SAVE);
         return filiationRepository.save(filiation);
     }
 
