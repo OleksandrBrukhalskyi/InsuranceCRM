266a43429510751400efa54cda0a6e81b6e729bb
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 04:59:28 +0200

    Added new error message about customer.

--- src/main/java/com/insurance/crm/constant/ErrorMessage.java
+++ src/main/java/com/insurance/crm/constant/ErrorMessage.java
@@ -3,4 +3,5 @@ package com.insurance.crm.constant;
 public class ErrorMessage {
     public static final String AGENT_NOT_FOUND_BY_ID = "The agent does not exist by this id: ";
     public static final String AGENT_NOT_FOUND_BY_EMAIL = "The agent does not exist by this email: ";
+    public static final String CUSTOMER_NOT_FOUND_BY_ID = "The customer does not exist by this id: ";
 }
