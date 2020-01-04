b44906d29da30421e5f7f201acc5f7e1a318df10
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 05:03:48 +0200

    Added error message about Filiation entity.

--- src/main/java/com/insurance/crm/constant/ErrorMessage.java
+++ src/main/java/com/insurance/crm/constant/ErrorMessage.java
@@ -4,4 +4,5 @@ public class ErrorMessage {
     public static final String AGENT_NOT_FOUND_BY_ID = "The agent does not exist by this id: ";
     public static final String AGENT_NOT_FOUND_BY_EMAIL = "The agent does not exist by this email: ";
     public static final String CUSTOMER_NOT_FOUND_BY_ID = "The customer does not exist by this id: ";
+    public static final String FILIATION_NOT_FOUND_BY_ID = "The filiation does not exist by this id: ";
 }
