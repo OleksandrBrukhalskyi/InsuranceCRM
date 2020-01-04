2c59bc3e1bb51a4cef385bfbc55c634c380d3a2f
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 15:22:04 +0200

    Added Optional<T> to method of searching entity by id.

--- src/main/java/com/insurance/crm/service/CustomerService.java
+++ src/main/java/com/insurance/crm/service/CustomerService.java
@@ -1,17 +1,17 @@
 package com.insurance.crm.service;
 
 
-
 import com.insurance.crm.dto.customer.CustomerDto;
 import com.insurance.crm.entity.Customer;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CustomerService {
     List getCustomers();
     Customer create(Customer customer);
     Customer update(CustomerDto customer,Long id);
     void delete(Long id);
-    Customer getById(Long id);
+    Optional<Customer> getById(Long id);
     Customer findByCustomerSurname(String surname);
 }
