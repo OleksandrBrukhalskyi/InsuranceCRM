61c17747da3548e2cfafacd8626ecbff4600da66
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 04:46:34 +0200

    Added FiliationDto as parameter to Filiation update method

--- src/main/java/com/insurance/crm/service/FiliationService.java
+++ src/main/java/com/insurance/crm/service/FiliationService.java
@@ -1,5 +1,6 @@
 package com.insurance.crm.service;
 
+import com.insurance.crm.dto.filiation.FiliationDto;
 import com.insurance.crm.entity.Filiation;
 
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.Optional;
 public interface FiliationService {
     List<Filiation> getFiliations();
     Filiation create(Filiation filiation);
-    Filiation update(Filiation filiation);
+    Filiation update(FiliationDto filiation,Long id);
     void delete(Long id);
     Optional<Filiation> findById(Long id);
 }
