92b9f7c9ac31b6c302990040d82891f7b42f7c54
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 05:11:13 +0200

    Implemented update method in FiliationService.

--- src/main/java/com/insurance/crm/service/impl/FiliationServiceImpl.java
+++ src/main/java/com/insurance/crm/service/impl/FiliationServiceImpl.java
@@ -1,6 +1,8 @@
 package com.insurance.crm.service.impl;
 
+import com.insurance.crm.dto.filiation.FiliationDto;
 import com.insurance.crm.entity.Filiation;
+import com.insurance.crm.exception.BadIdException;
 import com.insurance.crm.repository.FiliationRepository;
 import com.insurance.crm.service.FiliationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
+import static com.insurance.crm.constant.ErrorMessage.FILIATION_NOT_FOUND_BY_ID;
 
 @Service
 public class FiliationServiceImpl implements FiliationService {
@@ -25,9 +29,16 @@ public class FiliationServiceImpl implements FiliationService {
     }
 
     @Override
-    public Filiation update(Filiation filiation) {
+    public Filiation update(FiliationDto dto, Long id) {
+        Filiation filiation = filiationRepository.findById(id)
+                .orElseThrow(()-> new BadIdException(FILIATION_NOT_FOUND_BY_ID + id));
+        filiation.setName(dto.getName());
+        filiation.setAddress(dto.getAddress());
+        filiation.setPhone(dto.getPhone());
         return filiationRepository.save(filiation);
     }
+
+
 
     @Override
     public void delete(Long id) {
