70dfa91875dfb034c3f3f725c7d746b936f53102
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 15:11:55 +0200

    Deleted Optional<T> from method of searching entity by id.

--- src/main/java/com/insurance/crm/service/CustomerService.java
+++ src/main/java/com/insurance/crm/service/CustomerService.java
@@ -6,13 +6,12 @@ import com.insurance.crm.dto.customer.CustomerDto;
 import com.insurance.crm.entity.Customer;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CustomerService {
     List getCustomers();
     Customer create(Customer customer);
     Customer update(CustomerDto customer,Long id);
     void delete(Long id);
-    Optional<Customer> getById(Long id);
+    Customer getById(Long id);
     Customer findByCustomerSurname(String surname);
 }
