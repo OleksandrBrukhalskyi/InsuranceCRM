9e691a145864c014e857c9ff8df3c97a650e1799
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 11:45:15 +0200

    Extends RuntimeException in NotFoundException.

--- src/main/java/com/insurance/crm/exception/NotFoundException.java
+++ src/main/java/com/insurance/crm/exception/NotFoundException.java
@@ -1,4 +1,7 @@
 package com.insurance.crm.exception;
 
-public class NotFoundException {
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
 }
