c0919958f36f5c5167caa22d24d0729e65868e9e
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 12:28:09 +0200

    Added NotSaved,NotUpdated,NotDeleted error messages about InsurancePolicy in ErrorMessage.

--- src/main/java/com/insurance/crm/constant/ErrorMessage.java
+++ src/main/java/com/insurance/crm/constant/ErrorMessage.java
@@ -11,6 +11,13 @@ public class ErrorMessage {
     public static final String CUSTOMER_NOT_UPDATED ="The customer not updated";
     public static final String CUSTOMER_NOT_SAVED_BY_SURNAME ="The customer not saved by this surname";
     public static final String FILIATION_NOT_FOUND_BY_ID = "The filiation does not exist by this id: ";
+    public static final String FILIATION_NOT_DELETED ="The filiation not deleted";
+    public static final String FILIATION_NOT_UPDATED ="The filiation not updated";
+    public static final String FILIATION_NOT_SAVED_BY_NAME ="The filiation not saved by this name";
     public static final String INSURANCE_POLICY_NOT_FOUND_BY_ID = "The insurance policy does not exist by this id: ";
+    public static final String INSURANCE_POLICY_NOT_DELETED = "The insurance policy not deleted";
+    public static final String INSURANCE_POLICY_NOT_UPDATED = "The insurance policy not updated";
     public static final String INSURANCE_TYPE_NOT_FOUND_BY_ID = "The insurance type does not exist by this id: ";
+
+
 }
