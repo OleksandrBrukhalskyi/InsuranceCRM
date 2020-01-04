371e67a0234bf8d7233aa0347d531afaa80409be
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 05:57:58 +0200

    Created LogMessage class for logging.

---
+++ src/main/java/com/insurance/crm/constant/LogMessage.java
@@ -0,0 +1,9 @@
+package com.insurance.crm.constant;
+
+public class LogMessage {
+    public static final String IN_SAVE = "in save(), entity: {}";
+    public static final String IN_FIND_BY_ID = "in findById(), id: {}";
+    public static final String IN_UPDATE = "in update(), updated entity: {}";
+    public static final String IN_DELETE_BY_ID = "in deleteById(), id: {}";
+    public static final String IN_FIND_ALL = "in findAll()";
+}
