42e36af9a614ee41f05b827f7b185535bf5c611e
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 04:49:09 +0200

    Added InsurancePolicyUpdateDto as parameter to InsurancePolicy update method.

--- src/main/java/com/insurance/crm/service/InsurancePolicyService.java
+++ src/main/java/com/insurance/crm/service/InsurancePolicyService.java
@@ -1,5 +1,6 @@
 package com.insurance.crm.service;
 
+import com.insurance.crm.dto.policy.InsurancePolicyUpdateDto;
 import com.insurance.crm.entity.InsurancePolicy;
 
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.Optional;
 public interface InsurancePolicyService {
     List<InsurancePolicy> getInsurancePolicies();
     InsurancePolicy create(InsurancePolicy insurancePolicy);
-    InsurancePolicy update(InsurancePolicy insurancePolicy);
+    InsurancePolicy update(InsurancePolicyUpdateDto insurancePolicy,Long id);
     void delete(Long id);
     Optional<InsurancePolicy> findById(Long id);
     List<InsurancePolicy> getInsurancePoliciesByInsuranceTypeTag(String tag);
