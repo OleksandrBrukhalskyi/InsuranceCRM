3d5ade72bbf34e68e809c0542ce827c432dc5e68
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 05:30:20 +0200

    Implemented update method in InsuranceTypeService.

--- src/main/java/com/insurance/crm/service/impl/InsuranceTypeServiceImpl.java
+++ src/main/java/com/insurance/crm/service/impl/InsuranceTypeServiceImpl.java
@@ -1,6 +1,8 @@
 package com.insurance.crm.service.impl;
 
+import com.insurance.crm.dto.type.InsuranceTypeUpdateDto;
 import com.insurance.crm.entity.InsuranceType;
+import com.insurance.crm.exception.BadIdException;
 import com.insurance.crm.repository.InsuranceTypeRepository;
 import com.insurance.crm.service.InsuranceTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
+import static com.insurance.crm.constant.ErrorMessage.INSURANCE_POLICY_NOT_FOUND_BY_ID;
 
 @Service
 public class InsuranceTypeServiceImpl implements InsuranceTypeService {
@@ -25,9 +29,16 @@ public class InsuranceTypeServiceImpl implements InsuranceTypeService {
     }
 
     @Override
-    public InsuranceType update(InsuranceType insuranceType) {
+    public InsuranceType update(InsuranceTypeUpdateDto dto, Long id) {
+        InsuranceType insuranceType = insuranceTypeRepository.findById(id)
+                .orElseThrow(()-> new BadIdException(INSURANCE_POLICY_NOT_FOUND_BY_ID + id));
+        insuranceType.setName(dto.getName());
+        insuranceType.setTag(dto.getTag());
+        insuranceType.setSumInsured(dto.getSumInsured());
         return insuranceTypeRepository.save(insuranceType);
     }
+
+
 
     @Override
     public void delete(Long id) {
