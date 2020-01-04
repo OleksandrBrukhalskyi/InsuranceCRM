79391c4e0a1106964913c6bf6bc8a2eced170f8f
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 05:07:19 +0200

    Added error messages about InsurancePolicy and InsuranceType entities.

--- src/main/java/com/insurance/crm/constant/ErrorMessage.java
+++ src/main/java/com/insurance/crm/constant/ErrorMessage.java
@@ -5,4 +5,6 @@ public class ErrorMessage {
     public static final String AGENT_NOT_FOUND_BY_EMAIL = "The agent does not exist by this email: ";
     public static final String CUSTOMER_NOT_FOUND_BY_ID = "The customer does not exist by this id: ";
     public static final String FILIATION_NOT_FOUND_BY_ID = "The filiation does not exist by this id: ";
+    public static final String INSURANCE_POLICY_NOT_FOUND_BY_ID = "The insurance policy does not exist by this id: ";
+    public static final String INSURANCE_TYPE_NOT_FOUND_BY_ID = "The insurance type does not exist by this id: ";
 }
