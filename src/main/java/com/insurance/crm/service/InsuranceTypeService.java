d15561b67077c9a6f747b278f54d14ac7a57ba0f
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 19:55:23 +0200

    Added logging and mapping in InsuranceTypeService

--- src/main/java/com/insurance/crm/service/InsuranceTypeService.java
+++ src/main/java/com/insurance/crm/service/InsuranceTypeService.java
@@ -1,6 +1,7 @@
 package com.insurance.crm.service;
 
 import com.insurance.crm.dto.type.InsuranceTypeUpdateDto;
+import com.insurance.crm.dto.type.InsuranceTypeСreationDto;
 import com.insurance.crm.entity.InsuranceType;
 
 import java.util.List;
@@ -8,8 +9,8 @@ import java.util.Optional;
 
 public interface InsuranceTypeService {
     List<InsuranceType> getInsuranceTypes();
-    InsuranceType create(InsuranceType insuranceType);
-    InsuranceType update(InsuranceTypeUpdateDto insuranceType,Long id);
+    InsuranceType create(InsuranceTypeСreationDto dto);
+    InsuranceType update(InsuranceTypeUpdateDto dto,Long id);
     void delete(Long id);
     Optional<InsuranceType> findById(Long id);
     List<InsuranceType> getInsuranceTypeByName(String name);
--- src/main/java/com/insurance/crm/service/impl/InsuranceTypeServiceImpl.java
+++ src/main/java/com/insurance/crm/service/impl/InsuranceTypeServiceImpl.java
@@ -1,37 +1,50 @@
 package com.insurance.crm.service.impl;
 
+import com.insurance.crm.constant.ErrorMessage;
+import com.insurance.crm.constant.LogMessage;
 import com.insurance.crm.dto.type.InsuranceTypeUpdateDto;
+import com.insurance.crm.dto.type.InsuranceTypeСreationDto;
 import com.insurance.crm.entity.InsuranceType;
 import com.insurance.crm.exception.BadIdException;
 import com.insurance.crm.repository.InsuranceTypeRepository;
 import com.insurance.crm.service.InsuranceTypeService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-import static com.insurance.crm.constant.ErrorMessage.INSURANCE_POLICY_NOT_FOUND_BY_ID;
+
 
 @Service
+@AllArgsConstructor
+@Slf4j
 public class InsuranceTypeServiceImpl implements InsuranceTypeService {
     @Autowired
     InsuranceTypeRepository insuranceTypeRepository;
+    private ModelMapper modelMapper;
 
     @Override
     public List<InsuranceType> getInsuranceTypes() {
+        log.info(LogMessage.IN_FIND_ALL);
         return insuranceTypeRepository.findAll();
+
     }
 
     @Override
-    public InsuranceType create(InsuranceType insuranceType) {
-        return insuranceTypeRepository.save(insuranceType);
+    public InsuranceType create(InsuranceTypeСreationDto dto) {
+        log.info(LogMessage.IN_SAVE,dto);
+        return insuranceTypeRepository.save(modelMapper.map(dto,InsuranceType.class));
     }
 
     @Override
     public InsuranceType update(InsuranceTypeUpdateDto dto, Long id) {
+        log.info(LogMessage.IN_UPDATE,dto);
         InsuranceType insuranceType = insuranceTypeRepository.findById(id)
-                .orElseThrow(()-> new BadIdException(INSURANCE_POLICY_NOT_FOUND_BY_ID + id));
+                .orElseThrow(()-> new BadIdException(ErrorMessage.INSURANCE_POLICY_NOT_FOUND_BY_ID + id));
         insuranceType.setName(dto.getName());
         insuranceType.setTag(dto.getTag());
         insuranceType.setSumInsured(dto.getSumInsured());
@@ -42,16 +55,19 @@ public class InsuranceTypeServiceImpl implements InsuranceTypeService {
 
     @Override
     public void delete(Long id) {
+        log.info(LogMessage.IN_DELETE_BY_ID,id);
         insuranceTypeRepository.deleteById(id);
     }
 
     @Override
     public Optional<InsuranceType> findById(Long id) {
+        log.info(LogMessage.IN_FIND_BY_ID,id);
         return insuranceTypeRepository.findById(id);
     }
 
     @Override
     public List<InsuranceType> getInsuranceTypeByName(String name) {
+        log.info(LogMessage.IN_FIND_BY_INSURANCE_TYPE_NAME,name);
         return insuranceTypeRepository.getInsuranceTypeByName(name);
     }
 }
