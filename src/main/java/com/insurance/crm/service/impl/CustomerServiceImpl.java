52f851c32f001957bfc06dd0d04b8372fd811993
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 05:01:39 +0200

    Implemented update method in CustomerService.

--- src/main/java/com/insurance/crm/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/insurance/crm/service/impl/CustomerServiceImpl.java
@@ -1,6 +1,8 @@
 package com.insurance.crm.service.impl;
 
+import com.insurance.crm.dto.customer.CustomerDto;
 import com.insurance.crm.entity.Customer;
+import com.insurance.crm.exception.BadIdException;
 import com.insurance.crm.repository.CustomerRepository;
 import com.insurance.crm.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
+import static com.insurance.crm.constant.ErrorMessage.CUSTOMER_NOT_FOUND_BY_ID;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
@@ -25,9 +29,19 @@ public class CustomerServiceImpl implements CustomerService {
     }
 
     @Override
-    public Customer update(Customer customer) {
+    public Customer update(CustomerDto dto, Long id) {
+        Customer customer = customerRepository.findById(id)
+                .orElseThrow(()-> new BadIdException(CUSTOMER_NOT_FOUND_BY_ID + id));
+        customer.setSurname(dto.getSurname());
+        customer.setFirstname(dto.getFirstname());
+        customer.setPatronymic(dto.getPatronymic());
+        customer.setHomeAddress(dto.getHomeAddress());
+        customer.setPhoneNum(dto.getHomeNumber());
+        customer.setAge(dto.getAge());
         return customerRepository.save(customer);
     }
+
+
 
     @Override
     public void delete(Long id) {
