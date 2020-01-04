1710b9e404300134973aa4acc848f2f5063b6365
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 16:27:52 +0200

    Added log message of searching policies by insuranceType tag.

--- src/main/java/com/insurance/crm/constant/LogMessage.java
+++ src/main/java/com/insurance/crm/constant/LogMessage.java
@@ -8,4 +8,5 @@ public class LogMessage {
     public static final String IN_FIND_ALL = "in findAll()";
     public static final String IN_FIND_BY_EMAIL = "in getByEmail(), email: {}";
     public static final String IN_FIND_BY_SURNAME ="in findBySurname(), surname: {}";
+    public static final String IN_FIND_BY_TAG = "in findInsurancePoliciesByInsuranceTypeTag(), tag: {}";
 }
