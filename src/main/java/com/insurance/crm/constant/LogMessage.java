17d5b007751aca04748e2f039b892a4b8dfebfcb
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 06:13:54 +0200

    Added new IN_FIND_BY_EMAIL log message.

--- src/main/java/com/insurance/crm/constant/LogMessage.java
+++ src/main/java/com/insurance/crm/constant/LogMessage.java
@@ -6,4 +6,5 @@ public class LogMessage {
     public static final String IN_UPDATE = "in update(), updated entity: {}";
     public static final String IN_DELETE_BY_ID = "in deleteById(), id: {}";
     public static final String IN_FIND_ALL = "in findAll()";
+    public static final String IN_FIND_BY_EMAIL = "in getByEmail(), email: {}";
 }
