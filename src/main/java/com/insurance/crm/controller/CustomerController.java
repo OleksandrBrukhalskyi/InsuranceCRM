6d001692298798f4dab498c788327d5561b8b971
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 06:30:42 +0200

    Added Customer save method in CustomerController.

--- src/main/java/com/insurance/crm/controller/CustomerController.java
+++ src/main/java/com/insurance/crm/controller/CustomerController.java
@@ -1,7 +1,35 @@
 package com.insurance.crm.controller;
 
+import com.insurance.crm.constant.HttpStatuses;
+import com.insurance.crm.entity.Customer;
+import com.insurance.crm.service.CustomerService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
+    @Autowired
+    CustomerService customerService;
+
+    @ApiOperation(value = "Save customer")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = HttpStatuses.OK),
+            @ApiResponse(code = 303, message = HttpStatuses.SEE_OTHER),
+            @ApiResponse(code = 403, message = HttpStatuses.FORBIDDEN),
+    })
+    @PostMapping
+    public ResponseEntity save(@Valid @RequestBody Customer customer){
+        return ResponseEntity.status(HttpStatus.CREATED).body(customerService.create(customer));
+    }
 }
