d26772697ee77d38881819ac919ad80731755199
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 18:19:56 +0200

    Implemented create Filiation method in FiliationController.

--- src/main/java/com/insurance/crm/controller/FiliationController.java
+++ src/main/java/com/insurance/crm/controller/FiliationController.java
@@ -1,10 +1,21 @@
 package com.insurance.crm.controller;
 
+import com.insurance.crm.constant.HttpStatuses;
+import com.insurance.crm.dto.filiation.FiliationDto;
 import com.insurance.crm.service.impl.FiliationServiceImpl;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/filiation")
@@ -12,4 +23,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class FiliationController {
     private FiliationServiceImpl filiationService;
     private ModelMapper modelMapper;
+
+    @ApiOperation(value = "Create Filiation")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = HttpStatuses.OK),
+            @ApiResponse(code = 303, message = HttpStatuses.SEE_OTHER),
+            @ApiResponse(code = 400, message = HttpStatuses.BAD_REQUEST),
+            @ApiResponse(code = 403, message = HttpStatuses.FORBIDDEN)
+    })
+    @PostMapping
+    public ResponseEntity save(@Valid @RequestBody FiliationDto dto){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(filiationService.create(dto));
+    }
 }
