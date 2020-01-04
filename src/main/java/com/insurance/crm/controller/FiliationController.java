94ce798fef924a1d9e7e47f1b95a1f7c9bdd59e0
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-04 18:13:39 +0200

    Created FiliationController.

---
+++ src/main/java/com/insurance/crm/controller/FiliationController.java
@@ -0,0 +1,12 @@
+package com.insurance.crm.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/filiation")
+@AllArgsConstructor
+public class FiliationController {
+
+}
