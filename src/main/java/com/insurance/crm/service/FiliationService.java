f4d774011522c32ad7661ea4caf0b1e756326537
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-05 15:47:11 +0200

    fix: getById method which returned null

--- src/main/java/com/insurance/crm/service/FiliationService.java
+++ src/main/java/com/insurance/crm/service/FiliationService.java
@@ -4,12 +4,11 @@ import com.insurance.crm.dto.filiation.FiliationDto;
 import com.insurance.crm.entity.Filiation;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface FiliationService {
     List getFiliations();
     Filiation create(FiliationDto filiation);
     Filiation update(FiliationDto filiation,Long id);
     void delete(Long id);
-    Optional<FiliationDto> findById(Long id);
+    FiliationDto findById(Long id);
 }
