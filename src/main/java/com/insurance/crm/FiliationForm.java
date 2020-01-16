692d10976e4a23294da835de8eb70945827298a0
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-16 17:14:46 +0200

    feat: create FiliationForm.

---
+++ src/main/java/com/insurance/crm/FiliationForm.java
@@ -0,0 +1,14 @@
+package com.insurance.crm;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FiliationForm {
+    private String id;
+    private String name;
+    private String address;
+    private String phone;
+
+}
