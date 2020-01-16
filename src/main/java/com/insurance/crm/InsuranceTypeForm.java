49e43cb36d85f61471f176ecacefd8da6d89f20a
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-16 17:09:35 +0200

    feat: create InsuranceTypeForm.

---
+++ src/main/java/com/insurance/crm/InsuranceTypeForm.java
@@ -0,0 +1,13 @@
+package com.insurance.crm;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InsuranceTypeForm {
+    private String id;
+    private String name;
+    private String tag;
+    private double sumInsured;
+}
