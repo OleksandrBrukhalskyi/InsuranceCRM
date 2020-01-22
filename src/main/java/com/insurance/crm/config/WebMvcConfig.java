365dd9893c5a93fb689aab00623b04f5fc0e8b71
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-22 19:56:01 +0200

    use SonarQube for code quality. fix:Rename this field "MAX_AGE_SECS" to match the regular expression '^[a-z][a-zA-Z0-9]*$'. (SonarQube report).

--- src/main/java/com/insurance/crm/config/WebMvcConfig.java
+++ src/main/java/com/insurance/crm/config/WebMvcConfig.java
@@ -5,12 +5,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    private static final long MAX_AGE_SECS = 3600;
+    private static final long max_age_secs = 3600;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
-                .maxAge(MAX_AGE_SECS);
+                .maxAge(max_age_secs);
     }
 }
