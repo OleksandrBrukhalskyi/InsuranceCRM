34c9e385ca4bf0fde09e8c7229e50f4b53f8f988
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-22 19:35:59 +0200

    feat: drop Optional.

--- src/main/java/com/insurance/crm/service/AgentService.java
+++ src/main/java/com/insurance/crm/service/AgentService.java
@@ -4,14 +4,13 @@ import com.insurance.crm.entity.Agent;
 import com.insurance.crm.entity.Role;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface AgentService {
     List<Agent> getAgents();
     Agent create(Agent agent);
     Agent update(Agent agent);
     void delete(Long id);
-    Optional<Agent> getById(Long id);
+    Agent getById(Long id);
     Agent getByEmail(String email);
     Agent findByAgentSurname(String surname);
     List<Role> getRoles();
