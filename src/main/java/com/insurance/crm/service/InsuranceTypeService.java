b09810de201949e0242b6ab878ba4bc0c8433853
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-22 19:37:59 +0200

    feat: drop Optional.

--- src/main/java/com/insurance/crm/service/InsuranceTypeService.java
+++ src/main/java/com/insurance/crm/service/InsuranceTypeService.java
@@ -3,14 +3,13 @@ package com.insurance.crm.service;
 import com.insurance.crm.entity.InsuranceType;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface InsuranceTypeService {
     List<InsuranceType> getInsuranceTypes();
     InsuranceType create(InsuranceType dto);
     InsuranceType update(InsuranceType dto);
     void delete(Long id);
-    Optional<InsuranceType> findById(Long id);
+    InsuranceType findById(Long id);
     List<InsuranceType> getInsuranceTypeByName(String name);
 
 }
