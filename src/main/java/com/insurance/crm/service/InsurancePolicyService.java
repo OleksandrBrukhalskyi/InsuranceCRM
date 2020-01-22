f7a0986df0524011d38322b0072721ab3f71384e
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-22 19:37:26 +0200

    feat: add AgentPrincipal in methods.

--- src/main/java/com/insurance/crm/service/InsurancePolicyService.java
+++ src/main/java/com/insurance/crm/service/InsurancePolicyService.java
@@ -1,15 +1,16 @@
 package com.insurance.crm.service;
 
 import com.insurance.crm.entity.InsurancePolicy;
+import com.insurance.crm.security.entity.AgentPrincipal;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface InsurancePolicyService {
-    List<InsurancePolicy> getInsurancePolicies();
+    List<InsurancePolicy> getInsurancePolicies(AgentPrincipal agentPrincipal);
     InsurancePolicy create(InsurancePolicy dto);
     InsurancePolicy update(InsurancePolicy dto);
-    void delete(Long id);
-    Optional<InsurancePolicy> findById(Long id);
-    List<InsurancePolicy> getInsurancePoliciesByInsuranceTypeTag(String tag);
+    void delete(Long id,AgentPrincipal agentPrincipal);
+    Optional<InsurancePolicy> findById(Long id,AgentPrincipal agentPrincipal);
+    List<InsurancePolicy> getInsurancePoliciesByInsuranceTypeTag(String tag,AgentPrincipal agentPrincipal);
 }
