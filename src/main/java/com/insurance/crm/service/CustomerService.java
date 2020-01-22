400625073b192ec1db01c50ac68be91cbf9a0a02
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-22 19:36:40 +0200

    feat: drop Optional.

--- src/main/java/com/insurance/crm/service/CustomerService.java
+++ src/main/java/com/insurance/crm/service/CustomerService.java
@@ -4,13 +4,12 @@ package com.insurance.crm.service;
 import com.insurance.crm.entity.Customer;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CustomerService {
     List<Customer> getCustomers();
     Customer create(Customer customer);
     Customer update(Customer customer);
     void delete(Long id);
-    Optional<Customer> getById(Long id);
+    Customer getById(Long id);
     Customer findByCustomerSurname(String surname);
 }
