8cce425b8375377cb93fec2fd6edea84bf3b3882
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-23 21:16:45 +0200

    fix: Add a private constructor to hide the implicit public one(SonarQube report)

--- src/main/java/com/insurance/crm/constant/ErrorMessage.java
+++ src/main/java/com/insurance/crm/constant/ErrorMessage.java
@@ -1,6 +1,10 @@
 package com.insurance.crm.constant;
 
 public class ErrorMessage {
+
+    private ErrorMessage() {
+    }
+
     public static final String AGENT_NOT_FOUND_BY_ID = "The agent does not exist by this id: ";
     public static final String AGENT_NOT_FOUND_BY_EMAIL = "The agent does not exist by this email: ";
     public static final String AGENT_NOT_DELETED = "The agent not deleted: ";
