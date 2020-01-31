f5269941623214fbab393f2c6fc6cf44a8ac287c
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2020-01-31 09:36:18 +0200

    feat: create InsuranceTypeControllerTest.

---
+++ src/test/java/com/insurance/crm/controller/InsuranceTypeControllerTest.java
@@ -0,0 +1,45 @@
+package com.insurance.crm.controller;
+
+import org.junit.Test;
+
+public class InsuranceTypeControllerTest {
+
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void getInsuranceTypes() {
+
+    }
+
+    @Test
+    public void getById() {
+
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getQuantOfContractsGroupedByName() {
+    }
+
+    @Test
+    public void getTotal() {
+    }
+
+    @Test
+    public void getByGroupedSum() {
+    }
+
+    @Test
+    public void getByGroupedInsuranceTag() {
+    }
+}
